a349b4724f5ba0c4dbb7ded3a404494e445e0e32
Dewey Banks <DeweyBanks@gmail.com>
2016-07-27 16:27:07 -0400

    removes video

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:description, :image, :video)
+    params.require(:post).permit(:title, :body, :user_id, :image)
   end
 
 
